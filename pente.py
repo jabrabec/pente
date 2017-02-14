ad0677cbda177985beed494fcef1247cd332d47d
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-02-14 14:10:50 +0000

    check_for_five expanded into 4 different functions to separate single-line sequence checks (bug fix); added try/except and removed row/col constraints for recursion for better board coverage and ability to handle index out of range errors. check_for_winner no longer iterates over all possible board spaces but instead over the set of actual played spaces, which fixes for loop-bug and allows the game to quickly determine wins. This change also allowed if board[row][col] != ' .' statement to be removed from check_for_five as it is no longer necessary. Print statements and comments cleaned up.

--- pente.py
+++ pente.py
@@ -26,7 +26,7 @@ def make_board():
             # Assign x to each row
             board[row].append('.')
 
-    # Add a space to board at index [i][j] where value has only one character,
+    # Add a space to board at indices [i][j] where value has only one character
     # for improved visual spacing when printed. Will apply to both numeric
     # header values as well as individual cells marked by "."
     for i in range(len(board)):
@@ -38,7 +38,7 @@ def make_board():
 
 
 def print_board(board):
-    '''Print board in grid layout'''
+    '''Print board in a human-friendly grid layout'''
 
     for row in board:
         print " ".join(row)
@@ -83,14 +83,15 @@ def player_play(board, played=None):
             player_col = int(player_col)
 
     # ensure player picked an available space, add it to the set of used spaces
-    # set the marker at that space to 'X', print current state of board, check
+    # set the marker at that space to ' X', print current state of board, check
     # if winning move, and alternate to computer's turn to play
     if board[player_row][player_col] == ' .':
         played = played | {(player_row, player_col)}
         board[player_row][player_col] = ' X'
         print_board(board)
-        # if check_for_winner(board):
-        #     return
+        if check_for_winner(board, played):
+            print 'Game over!'
+            return
         computer_play(board, played)
     # reprompt player to choose again if they picked a nonempty space
     elif (player_row, player_col) in played:
@@ -120,76 +121,125 @@ def computer_play(board, played=None):
     comp_row = random.randrange(1, 16)
     comp_col = random.randrange(1, 16)
 
-    # ensure computer picked an available space, add it to the set of occupied,
-    # set the marker at that space to 'O', print current state of board, check
+    # ensure computer picked an available space, add it to the occupied set,
+    # set the marker at that space to ' O', print current state of board, check
     # if winning move, and alternate to player's turn to play
     if board[comp_row][comp_col] == ' .':
         played = played | {(comp_row, comp_col)}
         board[comp_row][comp_col] = ' O'
         print "The computer picked %s, %s." % (comp_row, comp_col)
         print_board(board)
-        # if check_for_winner(board):
-        #     return
+        if check_for_winner(board, played):
+            print 'Game over!'
+            return
         player_play(board, played)
     # call function again if computer picked a nonempty space
     elif (comp_row, comp_col) in played:
         computer_play(board, played)
 
 
-def check_for_five(board, count=0, row=1, col=1):
+def check_for_five_right(board, count=0, row=1, col=1):
     '''Iterate over board positions to check for wins'''
 
-    if board[row][col] != ' .':
-        count += 1
-    #     print 'not a ., count is %s' % (count)
-    # print 'value at row %s, col %s is %s, count is %s' % (
-    #     row, col, board[row][col], count)
-
-    # print 'iteration #: %s ' % (count)
     if count == 5:
         return True
 
-    # check right:
-    # left-most postion can't index past col. 11
-    if board[row][col] == board[row][col+1] and col < 12:
-        # print 'row: %s, col %s, count %s' % (row, col, count)
-        if check_for_five(board, count, row, col+1):
-            return True
-
-    # check down:
-    # top-most postion can't index past row 11
-    if board[row][col] == board[row+1][col] and row < 12:
-        # print 'row: %s, col %s, count %s' % (row, col, count)
-        if check_for_five(board, count, row+1, col):
-            return True
-
-    # check diagonally-down:
-    # top-most postion can't index past row 11 and col 11
-    if (board[row][col] == board[row+1][col+1] and row < 12 and col < 12):
-        # print 'row: %s, col %s, count %s' % (row, col, count)
-        if check_for_five(board, count, row+1, col+1):
-            return True
-
-    # check diagonally-up:
-    # left-most postion can't index before row 5 or after col 11
-    if (board[row][col] == board[row-1][col+1] and row > 4 and col < 12):
-        # print 'row: %s, col %s, count %s' % (row, col, count)
-        if check_for_five(board, count, row-1, col+1):
-            return True
+    # try/except added to handle index out-of-range errors, which removes
+    # necessity of coordinate checks for row/col values
+    try:
+        # check right:
+        if board[row][col] == board[row][col+1]:
+            count += 1
+            if check_for_five_right(board, count, row, col+1):
+                return True
+    except:
+        return False
 
     return False
 
 
-def check_for_winner(board):
-    """Can word be found in board?"""
+def check_for_five_down(board, count=0, row=1, col=1):
+    '''Iterate over board positions to check for wins'''
 
-    count = 0
-    for row in range(1, 16):
-        print 'checking row %s' % (row)
-        for col in range(1, 16):
-            print '\tchecking col %s' % (col)
-            if check_for_five(board, count, row, col):
+    if count == 5:
+        return True
+
+    # try/except added to handle index out-of-range errors, which removes
+    # necessity of coordinate checks for row/col values
+    try:
+        # check down:
+        if board[row][col] == board[row+1][col]:
+            count += 1
+            if check_for_five_down(board, count, row+1, col):
                 return True
+    except:
+        return False
+
+    return False
+
+
+def check_for_five_diag_down(board, count=0, row=1, col=1):
+    '''Iterate over board positions to check for wins'''
+
+    if count == 5:
+        return True
+
+    # try/except added to handle index out-of-range errors, which removes
+    # necessity of coordinate checks for row/col values
+    try:
+        # check diagonally-down:
+        if board[row][col] == board[row+1][col+1]:
+            count += 1
+            if check_for_five_diag_down(board, count, row+1, col+1):
+                return True
+    except:
+        return False
+
+    return False
+
+
+def check_for_five_diag_up(board, count=0, row=1, col=1):
+    '''Iterate over board positions to check for wins'''
+
+    if count == 5:
+        return True
+
+    # try/except added to handle index out-of-range errors, which removes
+    # necessity of coordinate checks for row/col values
+    try:
+        # check diagonally-up:
+        if board[row][col] == board[row-1][col+1]:
+            count += 1
+            if check_for_five_diag_up(board, count, row-1, col+1):
+                return True
+    except:
+        return False
+
+    return False
+
+
+def check_for_winner(board, played):
+    """Is there a consecutive sequence of 5 markers on the board?"""
+
+    # only check based on indices [row][col] where a marker actually exists
+    for i in sorted(list(played)):
+        # start count at 1 because first starting [row][col] space is by
+        # definition a positive match
+        count = 1
+        # row-col values to check correspond to (0, 1) in list of tuples
+        # converted from the set of spaces played.
+        row = i[0]
+        col = i[1]
+        # for all coordinates in played, check if they are part of a winning
+        # sequence of 5
+        if check_for_five_right(board, count, row, col):
+            return True
+        if check_for_five_down(board, count, row, col):
+            return True
+        if check_for_five_diag_down(board, count, row, col):
+            return True
+        if check_for_five_diag_up(board, count, row, col):
+            return True
     return False
 
 
